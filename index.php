689c39b4084921861d5dcb3dfd915f33d7fd5140
FURY438 <68443707+FURY438@users.noreply.github.com>
2022-11-13 22:10:25 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+echo '<h3>Москалик Василь, СП-41 (4.11.2022)</h3>';
+echo '<br>';
+echo '<img src="Screenshot_2.png"/>';
+
+echo '<form action="" method="post">
+    <input type="text" name="text" placeholder="Введіть текст:" >
+    <input type="submit" name="submit" value="Виконати">
+</from>';
+$regexp="%[a-zа-я]%";
+$str=$_POST["text"];
+if(preg_match_all($regexp,$str)){
+    $fp=fopen("file_1.txt","w+");
+    fwrite($fp,$str);
+    fclose($fp);
+
+    $readFile=fopen("file_1.txt","r");
+    $textFile=fgets($readFile);
+    fclose($readFile);
+
+    $arr = explode(" ", $textFile);
+    for ($i=0; $i<count($arr); $i++) {
+        $max = $arr[0];
+        if(strlen($arr[$i]) > strlen($max)){
+
+            $max = $arr[$i];
+        }
+    }
+    echo "<div><br>Найдовше слово у файлі: $max<br></div>";
+     $maxWordCount=substr_count($textFile,$max);
+    echo "<div><br>Це слово зустрічається $maxWordCount рази<br></div>";
+    }
+else
+    echo "Потрібно вводить тільки букви!!";
+?>
